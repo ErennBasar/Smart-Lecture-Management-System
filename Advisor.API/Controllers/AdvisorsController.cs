742a876b64d4a63b598df3089c32cb6166802310
ErennBasar <2200656020@nku.edu.tr>
2026-02-05 19:16:39 +0300

    Danışman ekleme endpoint'i oluşturuldu

--- Advisor.API/Controllers/AdvisorsController.cs
+++ Advisor.API/Controllers/AdvisorsController.cs
@@ -1,6 +1,9 @@
+using Advisor.API.DTOs;
+using Advisor.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Advisor.API.Controllers
 {
@@ -9,10 +12,49 @@ namespace Advisor.API.Controllers
     [ApiController]
     public class AdvisorsController : ControllerBase
     {
+        private readonly AdvisorDbContext _dbContext;
+
+        public AdvisorsController(AdvisorDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
             return Ok("Merhaba, ben Danışman Servisi. Pasaportun sağlam, içeri girdin.");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")] // Sadece rolü admin olanlar erişebilir
+        public async Task<IActionResult> CreateAdvisor(CreateAdvisorDto createAdvisorDto)
+        {
+            var existingAdvisor = await _dbContext.Advisors.FirstOrDefaultAsync(a =>
+                a.UserId == createAdvisorDto.UserId);
+
+            if (existingAdvisor != null)
+                return Conflict("BU Id'ye sahip danışman zaten mevcut");
+            
+            var newAdvisor = new Models.Advisor
+            {
+                Id = Guid.NewGuid(),
+                UserId = createAdvisorDto.UserId,
+                FirstName = createAdvisorDto.FirstName,
+                LastName = createAdvisorDto.LastName,
+                Email = createAdvisorDto.Email,
+                EmployeeNumber = createAdvisorDto.EmployeeNumber,
+                Department = createAdvisorDto.Department,
+                Specialization = createAdvisorDto.Specialization
+            };
+
+            _dbContext.Advisors.Add(newAdvisor);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Danışman başarıyla hoca oluşturuldu.",
+                advisorId = newAdvisor.Id
+            });
         }
     }
 }
---
+++ Advisor.API/DTOs/CreateAdvisorDto.cs
@@ -0,0 +1,12 @@
+namespace Advisor.API.DTOs;
+
+public class CreateAdvisorDto
+{
+    public Guid UserId { get; set; } // Identity tarafındaki GUID
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string EmployeeNumber { get; set; } = string.Empty; 
+    public string? Department { get; set; }
+    public string? Specialization { get; set; } 
+}
